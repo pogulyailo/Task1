bc45a90a2877e8a332e917e4682d2f2eed1c7a92
igr <mixa_fc@ukr.net>
2017-09-04 21:26:17 +0300

    первая программа

---
+++ src/task1.java
@@ -0,0 +1,8 @@
+    public class task1 {
+        public static void main(String[] args) {
+            int a = 24;
+            int b = 5;
+
+            System.out.println(((int ) a / (int) b) + "остаток" + ((int ) a % (int) b));
+        }
+    }
